918124d499b098e0356cc97a28d1be6b14e3df66
errno-mmd <errno-mmd@users.noreply.github.com>
2019-09-22 16:59:37 +0900

    skip installation of unused data

--- VMD3d-install.bat
+++ VMD3d-install.bat
@@ -1,15 +1,6 @@
 call activate mmdmat || goto die
 set TAG="ver1.02.01"
 git clone --depth 1 -b %TAG% https://github.com/miu200521358/VMD-3d-pose-baseline-multi.git || goto die
-cd VMD-3d-pose-baseline-multi\data || goto die
-mkdir saved_sessions || goto die
-cd saved_sessions || goto die
-curl -L -O http://visual.cs.ucl.ac.uk/pubs/liftingFromTheDeep/res/init_session.tar.gz || goto die
-tar zxvf init_session.tar.gz || goto die
-rm init_session.tar.gz || goto die
-curl -L -O http://visual.cs.ucl.ac.uk/pubs/liftingFromTheDeep/res/prob_model.tar.gz || goto die
-tar zxvf prob_model.tar.gz || goto die
-rm prob_model.tar.gz || goto die
 
 @echo COMPLETE
 @pause -1
