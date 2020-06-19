f1f0284891a3191eb31927ee55071492f1bfbfeb
errno-mmd <errno-mmd@users.noreply.github.com>
2020-06-20 00:20:08 +0900

    don't use utf-8 encoding in setup

--- tool/setup.py
+++ tool/setup.py
@@ -242,7 +242,7 @@ class SetupFrame(wx.Frame):
 
     def run_command(self, command, **kwargs):
         sys.stdout.write(command + '\n')
-        self.subproc = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1, encoding='utf-8', **kwargs)
+        self.subproc = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1, **kwargs)
         while self.subproc.poll() is None:
             msg = self.subproc.stdout.readline()
             if msg:
